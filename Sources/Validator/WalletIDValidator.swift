bbf98edca3f224acb216ed3c4f3a99defdca1a2b
kk <342317003@qq.com>
2019-05-05 11:18:49 +0800

    优化

--- Sources/Validator/WalletIDValidator.swift
+++ Sources/Validator/WalletIDValidator.swift
@@ -11,6 +11,7 @@ import Foundation
 public class WalletIDValidator: Validator {
   typealias Result = WalletID
   private let walletID: WalletID
+  //匹配部分签名带有ETH此类ID
   private let formatRegex = "^[A-Za-f0-9]{8,11}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$"
 
   public init(walletID: WalletID) {
